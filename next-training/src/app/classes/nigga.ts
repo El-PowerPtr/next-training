f531ab411b13631069c2833ee7b6206ee8de3875
El-PowerPtr <moiseselpower@gmail.com>
2024-09-01 23:07:00 -0400

    start

---
+++ next-training/src/app/classes/nigga.ts
@@ -0,0 +1,11 @@
+export default class Nigga{
+    readonly name :string;
+    readonly age : number;
+    readonly phone: string[];
+
+    constructor(name:string, age: number, phone: string[]){
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+}
---
+++ next-training/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function name() {
+    return( <>
+        <h1>
+            Hola mundo
+        </h1>
+    </>);
+}
